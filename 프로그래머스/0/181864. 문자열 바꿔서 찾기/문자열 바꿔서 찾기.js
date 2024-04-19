b7b44b0e9ba18bf26253245a8ffc9cd80a64b6f2
sangyong park <76932869+sangyoung23@users.noreply.github.com>
2024-04-19 13:07:19 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -0,0 +1,8 @@
+function solution(myString, pat) {
+    const string = [...myString].reduce(
+    (acc, cur) => acc + (cur === 'A' ? 'B' : 'A'),
+    ''
+  );
+ 
+  return Number(string.includes(pat));
+}
